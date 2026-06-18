d8585144eee074ad6c93d3292460d6f40476f4b1
joegamal <yusufmagamalth@gmail.com>
2026-06-18 19:13:09 +0300

    borrowing

---
+++ functions.rs
@@ -0,0 +1,3 @@
+pub fn add_value(vec: &mut Vec<i32>) {
+    let _ = &vec.push(-10);
+}
--- main.rs
+++ main.rs
@@ -1,6 +1,14 @@
+mod functions;
+
 
 fn main() {
     let strng: String = String::from("hello world");
     
+    let mut vec = vec![0, 1, 2, 3, 4];
+    
+    let _ = functions::add_value(&mut vec);
+
+    println!("{}", vec.last().unwrap());
+
     println!("{}",strng);
 }
